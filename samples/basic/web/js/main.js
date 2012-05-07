c14c26f6a9332c15da104a6023627e38dabcd4c3
Yuri Dobronravin <logicking@logicking.com>
2012-05-07 22:58:45 +0300

    Sounds for basic sample

--- samples/basic/web/js/main.js
+++ samples/basic/web/js/main.js
@@ -1,5 +1,6 @@
 /**
  * Main.js
+ * Entry point of the whole game
  */
 
 // Entry point of the game
@@ -15,10 +16,19 @@ $(document).ready(function() {
 
 	//disable console
 	//console.log = function(){};
+
+	
 	// IMAGES
+	// in images/low we can put low-resolution images for the slower devices
 	Resources.addResolution("low", "images/low/");
 	Resources.addResolution("normal", "images/", true);
 
+	// Init sound-sprites audio
+	Sound.init("sounds/total", true, "js/");
+	Sound.add("click", "", 0, 0.3);
+	Sound.add("monkey", "", 1.0, 1.0);
+	Sound.add("final", "", 4.0, 2.0);
+	
 	Screen.init(Account.instance);
 
 	$(document)['bind']('keydown', function(e) {
@@ -31,6 +41,8 @@ $(document).ready(function() {
 		}
 	});
 
+	
+	// Initial state of the game - active MenuState
 	var data = {
 			"Account01" : {
 				"class" : "Account",
